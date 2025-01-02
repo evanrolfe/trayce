fe64ad4a1e6e2eb32f0b9eef41343672c4d03d00
Evan Rolfe <accounts@evanrolfe.me>
2025-01-02 08:56:37 +0000

    update goldens

--- test/network/containers_modal_test.dart
+++ test/network/containers_modal_test.dart
@@ -3,8 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ftrayce/agent/command_sender.dart';
 import 'package:ftrayce/agent/gen/api.pb.dart' as pb;
-import 'package:ftrayce/blocs/containers_cubit.dart';
-import 'package:ftrayce/network/containers_modal.dart';
+import 'package:ftrayce/network/bloc/containers_cubit.dart';
+import 'package:ftrayce/network/widgets/containers_modal.dart';
 
 class MockCommandSender implements CommandSender {
   @override
